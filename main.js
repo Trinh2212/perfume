bd2ad67d1d6efd540a054a05489dc009e0800d89
Diem Trinh <95289706+Trinh2212@users.noreply.github.com>
2024-11-09 12:19:11 +0700

    update 4

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-let navbar = document.querySelector('.navbar');
+const prices = document.querySelectorAll('.item-price');
+prices.forEach(item=> {
+    item.innerText=Number(item.innerText).toLocaleString('de-DE',{style: 'currency', currency : 'VND'})
+});
+
+let navbar = document.querySelector('.header__navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-let searchform = document.querySelector('.search-form');
+let searchform = document.querySelector('.header__search');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchform.classList.toggle('active');
 }
+
+
+
